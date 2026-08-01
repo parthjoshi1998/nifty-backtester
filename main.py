b6ee4b7a042c8bfb0810e0774d901eff08ff1c4f
parthjosh1998 <parthjoshi1998@outlook.com>
2026-08-02 02:16:36 +0530

    Initial project structure

---
+++ main.py
@@ -0,0 +1,9 @@
+from config import config
+from data import csv_loader
+from indicators import indicator_engine
+from filters import filter_engine
+from strategies import strategy_engine
+from risk import risk_engine
+from engine import backtest_engine
+
+print("✅ All imports successful!")
